9e47f0fa7bca585947adaaf96aa604b83bb57ac7
Mdsujansarkar <sujansarkar786@gmail.com>
2022-02-26 13:17:06 +0600

    refactor

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ require_once __DIR__ . '/vendor/autoload.php';
 use App\Product;
 use App\Unit;
 
-$ob = new Product();
+$ob = new Product(5.2,2.5);
 $obUnit = new Unit();
 echo $ob->getPrice($obUnit);
--- src/Product.php
+++ src/Product.php
@@ -7,10 +7,17 @@ class Product
 {
     const PATHO = 'patho';
     const DHL = 'dhl';
+    private $height;
+    private $length;
+    public function __construct($height, $length)
+    {
+        $this->height = $height;
+        $this->length = $length;
+    }
     public function getPrice(Unit $unit)
     {
         if(self::PATHO === 'patho') {
-            return ($unit->getWeight(2.5)*2) + ($unit->getVolume(2.3, 3.6)*3);
+            return ($unit->getWeight(2.5)*2) + ($unit->getVolume($this->height, $this->length)*3);
         }
         if(self::DHL === 'dhl') {
             return ($unit->getWeight(2.5)*3) +100;
